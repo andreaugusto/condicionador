67bde1de44a7da1445f94a33d5147f696a6bdaf6
André Augusto <andre.augustosc@gmail.com>
2013-06-15 23:07:27 -0300

    simulador: add argparser

--- simulador.py
+++ simulador.py
@@ -44,7 +44,41 @@ class Simulador:
         return (temperatura, custo_total)
 
 
+def parse_args():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Simulador para executar classe Condicionador"
+    )
+    parser.add_argument(
+        'minutos',
+        default=360,
+        nargs='?',
+        type=int,
+        help="Minutos que o simulador deve simular"
+    )
+    parser.add_argument(
+        'temp_atual',
+        default=30,
+        nargs='?',
+        type=int,
+        help="Temperatura atual que deve ser assumida pelo simulador"
+    )
+    parser.add_argument(
+        'temp_desejada',
+        default=20,
+        nargs='?',
+        type=int,
+        help="Temperatura que o simulador deve solicitar ao condicionador"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
 if __name__ == '__main__':
+    args = parse_args()
+
     logging.basicConfig(level=logging.INFO)
     simulador = Simulador()
-    simulador.executa(360, 30, 20)
+    simulador.executar(**vars(args))
